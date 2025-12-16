1d63d4692053139473925090c5c52042ff8f13e7
jorgesti <jorgen.stikbakke@nav.no>
2025-12-16 14:00:19 +0100

    rettet feil i vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        iframe: path.resolve(__dirname, 'iframe.html'),
+        main: 'index.html',
+        iframe: 'iframe.html',
       },
       output: {
         // Bruk standard navngivning hvis du ikke har spesielle grunner til custom dir
