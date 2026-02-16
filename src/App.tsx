7abb9bd086800d4b954f502f4841c2a8754eb570
jorgesti <jorgen.stikbakke@nav.no>
2026-02-16 09:45:21 +0100

    lagt til ny iframe for sensurering

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { AppLayout } from "./layout";
 import { SakerList } from "./pages/SakerList";
 import { SakDetail } from "./pages/SakDetail";
 import { NySak } from "./pages/NySak";
 import { RegistrerSak } from "./pages/RegistrerSak";
+import { SensureringIframe } from "./pages/SensureringIframe";
+
+const WithLayout = () => (
+  <AppLayout>
+    <Outlet />
+  </AppLayout>
+);
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
-      <AppLayout>
-        <Routes>
+      <Routes>
+        <Route path="/embed/sensurering/:sakId" element={<SensureringIframe />} />
+        <Route element={<WithLayout />}>
           <Route path="/" element={<SakerList />} />
           <Route path="/saker/ny" element={<NySak />} />
           <Route path="/registrer/:id" element={<RegistrerSak />} />
           <Route path="/saker/:id" element={<SakDetail />} />
           <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AppLayout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/SensureringIframe.tsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { SensureringEditor } from "../components/SensureringEditor";
+
+export const SensureringIframe = () => {
+  const { sakId } = useParams<{ sakId: string }>();
+
+  if (!sakId) {
+    return <p>Mangler sakId</p>;
+  }
+
+  return (
+    <div className="p-4">
+      <SensureringEditor
+        sakId={sakId}
+        onLagreOgLukk={() =>
+          window.parent.postMessage({ type: "sensurering-lagret" }, "*")
+        }
+      />
+    </div>
+  );
+};
